ec33f67e1527a715fb28178ba800877d9b454dba
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-01-23 21:27:04 +0500

    first comt

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let weatherIMG = document.querySelector(".weatherIMG");
 let sun =
   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII=";
 let cloud =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEkBsJ_sOq_8E_Ec-IPtCyUqJro9ZvAN8mpQ&s";
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBqfLiDiHRt_J__fCxdqV8i_tXLe0MsK7fmQ&s";
 let snow =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNtF54pw9NDfA2a5Eb6XYWHpeT3hNTvjnXpA&s";
 let rain =
@@ -24,24 +24,27 @@ cityInput.addEventListener("keyup", (e) => {
     .then((data) => data.json())
     .then((data) => {
       if (data.cod !== "404") {
-        console.log(data);
-        if (data.weather[0].main.toLowerCase() == "clouds") {
-          weatherIMG.src = cloud;
-        } else if (data.weather[0].main.toLowerCase() == "clear") {
-          weatherIMG.src = sun;
-        } else if (data.weather[0].main.toLowerCase() == "snow") {
-          weatherIMG.src = snow;
-        } else if (data.weather[0].main.toLowerCase() == "rain") {
-          weatherIMG.src = rain;
+        switch (data.weather[0].main.toLowerCase()) {
+          case "clouds":
+            weatherIMG.src = cloud;
+            break;
+          case "clear":
+            weatherIMG.src = sun;
+            break;
+          case "snow":
+            weatherIMG.src = snow;
+            break;
+          case "rain":
+            weatherIMG.src = rain;
+            break;
         }
         boxOfWeather.style.display = "block";
         city.innerHTML = data.name;
         condition.innerHTML = data.weather[0].main;
-        feelsLike.innerHTML = data.main.feels_like;
-        temperature.innerHTML = data.main.temp;
+        feelsLike.innerHTML = Math.round(data.main.feels_like - 273.15);
+        temperature.innerHTML = Math.round(data.main.temp - 273.15);
         humidity.innerHTML = data.main.humidity;
         windSpeed.innerHTML = data.wind.speed;
       }
-    })
-    .catch((err) => console.log(err, "error"));
+    });
 });
